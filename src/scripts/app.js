c7c30a40c94765f76de963cad11f25a2643ac4ee
Carlos Hernandez <carlos@technogi.com.mx>
2015-01-16 18:35:35 -0600

    jkj

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,19 +9,42 @@ var logger = require('./logger');
 var log = logger.create('app:log', 'log');
 var err = logger.create('app:err', 'error');
 
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 1){
+function adjust() {
+
+  if ($(this).scrollTop() > 1) {
     $('header').addClass("sticky");
     $("#main_content_footer").addClass("scrolled");
     $("#mobile_header_img").addClass("scrolled");
     $(".menu_item").addClass("menu_item_scrolled");
     $("#main_content_footer .row div.medium-3").addClass('medium-6').removeClass('medium-3');
   }
-  else{
+  else {
     $('header').removeClass("sticky");
     $("#main_content_footer").removeClass("scrolled");
     $("#mobile_header_img").removeClass("scrolled");
     $(".menu_item").removeClass("menu_item_scrolled");
     $("#main_content_footer .row div.medium-6").addClass('medium-3').removeClass('medium-6');
   }
+}
+
+
+function display_carousel_element(index, elements) {
+  $(elements[index]).slideDown(1000,function () {
+    setTimeout(function () {
+      $(elements[index]).slideUp(1000,function(){
+        index = (index === elements.length - 1) ? 0 : index + 1;
+        display_carousel_element(index, elements);
+      });
+    }, 5000);
+  });
+}
+
+function start_carousel() {
+  display_carousel_element(0, $(".display-element"));
+}
+
+$(window).load(function () {
+  adjust();
+  start_carousel();
 });
+$(window).scroll(adjust);
