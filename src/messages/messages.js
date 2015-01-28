4651a8326031c0903b77ea916ec7030d5c7ce98d
Carlos Hernandez <carlos@technogi.com.mx>
2015-01-28 11:57:35 -0600

    internacionnalisacion

--- src/messages/messages.js
+++ src/messages/messages.js
@@ -2,8 +2,14 @@ var messages = {
   "en": {
     "commons": {
       "learn_more": "learn more",
+      "send":"SEND",
       "sending":"sending ...",
       "thankyou":"Thank you"
+    },
+    "lang":{
+      "select":"idioma",
+      "spanish":"español",
+      "english":"english"
     },
     "views": {
       "header": {
@@ -23,56 +29,6 @@ var messages = {
           "description": "Lorem ipsum dolor sit amet, id regione impedit " +
             "definitiones usu, velit persius percipit no duo."
         },
-        "project_landing": {
-          "title": "Project Landing",
-          "description": "Servicio especializado en convertir ideas innovadoras " +
-            "y proyectos de alta complejidad en realidad. Utilizamos " +
-            "un proceso guiado que se adapta al estado actual de sus " +
-            "necesidades y garantiza el éxito de la implementación."
-        }
-
-      },
-      "about_us": {
-        "summary": "We are a consulting and development company built by professionals with over 10 years experience " +
-          "in implementing information technology solutions. Our commitment to research and innovate, allows us to " +
-          "provide our customers with simple solutions with the right technology, allowing them to focus on the " +
-          "productivity of their business."
-      },
-      "contact_us":{
-        "getintouch":"Get in touch",
-        "post_sent":"We'll get back to you within 24 hours. Hold on tight!",
-        "instructions":"Fill in the form below, and we'll get back to you within 24 hours.",
-        "our_office":"Our office"
-      },
-      "footer":{
-        "privacy_policy":"PRIVACY POLICY"
-      }
-    }
-  },
-  "es": {
-    "commons": {
-      "learn_more": "ver más",
-      "sending":"enviando ...",
-      "thankyou":"Gracias"
-    },
-    "views": {
-      "header": {
-        "services": "¿QUÉ HACEMOS?",
-        "methodology": "¿CÓMO LO HACEMOS?",
-        "contact": "CONTACTANOS"
-      },
-      "main": {
-        "h1": "Consultoría de TI novedosa",
-        "h2": "keep IT simple",
-        "msg": "Concéntrate en tu negocio, nosotros nos preocupamos por T.I."
-      },
-      "services": {
-        "h1": "Nuestros servicios",
-        "consulting": {
-          "title": "Consultoría IT",
-          "description": "Lorem ipsum dolor sit amet, id regione impedit " +
-            "definitiones usu, velit persius percipit no duo."
-        },
         "software_development": {
           "title": "Software Development",
           "description": "Servicio especializado en convertir ideas innovadoras " +
@@ -89,6 +45,127 @@ var messages = {
         },
         "project_landing": {
           "title": "Project Landing",
+          "description": "Specialized service that brings innovative ideas and high complexity proects to life." +
+            " We use a step by step process that adapts to your current business state and guarantees implementation" +
+            " success."
+        }
+      },
+      "about_us": {
+        "summary": "We are a consulting and development company built by professionals with over 10 years experience " +
+          "in implementing information technology solutions. Our commitment to research and innovate, allows us to " +
+          "provide our customers with simple solutions with the right technology, allowing them to focus on the " +
+          "productivity of their business."
+      },
+      "love_us":{
+        "h1":"This is Why Our Clients Love Us",
+        1:{
+          "title":"Client Satisfaction",
+          "desc":"Bacon ipsum dolor amet ribeye rump corned beef porchetta jowl pork chop landjaeger. Chuck pastrami" +
+            " turkey, picanha short ribs meatloaf ribeye. Brisket ground round beef alcatra, filet mignon rump " +
+            "frankfurter sausage ham hock."
+        },
+        2:{
+          "title":"Commitment",
+          "desc":"Bacon ipsum dolor amet ribeye rump corned beef porchetta jowl pork chop landjaeger. Chuck pastrami" +
+            " turkey, picanha short ribs meatloaf ribeye. Brisket ground round beef alcatra, filet mignon rump " +
+            "frankfurter sausage ham hock."
+        },
+        3:{
+          "title":"Trust",
+          "desc":"Bacon ipsum dolor amet ribeye rump corned beef porchetta jowl pork chop landjaeger. Chuck pastrami" +
+            " turkey, picanha short ribs meatloaf ribeye. Brisket ground round beef alcatra, filet mignon rump " +
+            "frankfurter sausage ham hock."
+        },
+        4:{
+          "title":"Less Risk",
+          "desc":"Bacon ipsum dolor amet ribeye rump corned beef porchetta jowl pork chop landjaeger. Chuck pastrami" +
+            " turkey, picanha short ribs meatloaf ribeye. Brisket ground round beef alcatra, filet mignon rump " +
+            "frankfurter sausage ham hock."
+        },
+        5:{
+          "title":"Visibility in Real Time",
+          "desc":"Bacon ipsum dolor amet ribeye rump corned beef porchetta jowl pork chop landjaeger. Chuck pastrami" +
+            " turkey, picanha short ribs meatloaf ribeye. Brisket ground round beef alcatra, filet mignon rump " +
+            "frankfurter sausage ham hock."
+        },
+        6:{
+          "title":"Top Talent",
+          "desc":"Bacon ipsum dolor amet ribeye rump corned beef porchetta jowl pork chop landjaeger. Chuck pastrami" +
+            " turkey, picanha short ribs meatloaf ribeye. Brisket ground round beef alcatra, filet mignon rump " +
+            "frankfurter sausage ham hock."
+        }
+      },
+      "our_way":{
+        "h1":"How We Do IT",
+        "philosophy":{
+          "title":"Our Philosophy",
+          "desc":"The latest technology with zero risk. We help you to evolve"
+        },
+        "team":{
+          "title":"The Team",
+          "desc":"Highly enthusiastic and Customer oriented. We love technology, we love what we do"
+        },
+        "methodology":{
+          "title":"Methodology",
+          "desc":"Because time to market is everything"
+        }
+      },
+      "contact_us":{
+        "getintouch":"Get in touch",
+        "post_sent":"We'll get back to you within 24 hours. Hold on tight!",
+        "instructions":"Fill in the form below, and we'll get back to you within 24 hours.",
+        "our_office":"Our office",
+        "visit_us":"visit us at our office"
+      },
+      "footer":{
+        "privacy_policy":"PRIVACY POLICY"
+      }
+    }
+  },
+  "es": {
+    "commons": {
+      "learn_more": "ver más",
+      "sending":"enviando ...",
+      "thankyou":"Gracias"
+    },
+    "lang":{
+      "select":"lang",
+      "spanish":"español",
+      "english":"english"
+    },
+    "views": {
+      "header": {
+        "services": "¿QUÉ HACEMOS?",
+        "methodology": "¿CÓMO LO HACEMOS?",
+        "contact": "CONTACTANOS"
+      },
+      "main": {
+        "h1": "Servicios de Consultoría IT Modernos",
+        "h2": "keep IT simple",
+        "msg": "Concéntrate en tu negocio, nosotros nos preocupamos por T.I."
+      },
+      "services": {
+        "h1": "Nuestros servicios",
+        "consulting": {
+          "title": "Consultoría IT",
+          "description": "Asesoría en el ciclo de implementación de soluciones de TI basadas en software. Participamos " +
+            "en los aspectos vitales de su proyecto: planificación, levantamiento de requerimientos, diseño, desarrollo," +
+            " pruebas, instalación y soporte post productivo."
+        },
+        "software_development": {
+          "title": "Desarrollo Software",
+          "description": "Desarrollo de aplicaciones web y móviles, integración de sistemas y componentes de negocio. " +
+            "Creamos la arquitectura ideal y utilizamos la tecnología que más se adapte a sus necesidades y que " +
+            "incremente la vida útil de la solución."
+        },
+        "cloud": {
+          "title": "Nube",
+          "description": "Asesoría especializada para aprovechamiento de las capacidades que ofrece la nube. " +
+            "Diagnosticamos el nivel de madurez de sus aplicaciones para incorporarlas a la nube y lo ayudamos a " +
+            "definir los pasos a seguir para comenzarla a utilizar."
+        },
+        "project_landing": {
+          "title": "Project Landing",
           "description": "Servicio especializado en convertir ideas innovadoras " +
             "y proyectos de alta complejidad en realidad. Utilizamos " +
             "un proceso guiado que se adapta al estado actual de sus " +
@@ -96,11 +173,67 @@ var messages = {
         }
 
       },
+      "love_us":{
+        "h1":"¿Por qué nos quieren nuestros clientes?",
+        1:{
+          "title":"Satisfacción al cliente",
+          "desc":"100% de los clientes han desarrollado más de un proyecto con nosotros."
+        },
+        2:{
+          "title":"Compromiso",
+          "desc":"	100% de nuestros clientes nos ven como un socio más de su proyecto."
+        },
+        3:{
+          "title":"Confianza",
+          "desc":"100% de nuestros clientes nos ven como un socio más de su proyecto."
+        },
+        4:{
+          "title":"Menor Riesgo",
+          "desc":"Anticipamos los riesgos y respondemos de manera ágil para resolver cualquier problema que se presente " +
+            "durante la ejecución del proyecto."
+        },
+        5:{
+          "title":"Visibilidad Constante",
+          "desc":"Los clientes saben exactamente en qué estamos trabajando, ven avances en tiempo real y saben en qué " +
+            "se está ocupando su dinero."
+        },
+        6:{
+          "title":"Recursos Elite",
+          "desc":"	Logramos la atracción de recursos top, de las universidades más importantes del país debido a la " +
+            "filosofía y las herramientas que utilizamos."
+        }
+      },
+      "our_way":{
+        "h1":"¿Cómo lo Hacemos?",
+        "philosophy":{
+          "title":"Filosofía",
+          "desc":"Nos caracterizamos por realizar investigación sobre nuevas tecnologías y formas de resolver los " +
+            "problemas. Llevamos los resultados de la investigación a la práctica, creando productos propios que nos " +
+            "permitan entender la tecnología que se está creando. Una vez que somos especialistas, implantamos las " +
+            "soluciones con nuestros clientes. Lo anterior nos permite proveer a nuestros clientes con un catálogo " +
+            "extenso de posibilidades para convertir sus ideas en soluciones."
+        },
+        "team":{
+          "title":"El equipo",
+          "desc":"Nuestro equipo está conformado por especialistas y profesionales con alto potencial que se han " +
+            "desempeñado en diversos ámbitos del desarrollo de soluciones de TI. Su compromiso, dedicación y constante " +
+            "deseo por innovar, hacen de nuestro equipo un elemento vital para producir las soluciones competitivas " +
+            "que nuestros clientes necesitan."
+        },
+        "methodology":{
+          "title":"La Metodología",
+          "desc":"Con las mejores prácticas del PMI y de las metodologías ágiles, hemos creado una forma de trabajo que " +
+            "estimula la productividad de nuestro equipo e incrementa la comunicación con el cliente, dándonos la " +
+            "combinación ideal de control, visibilidad y flexibilidad que todo proceso de desarrollo requiere. El " +
+            "resultado, 100% de clientes satisfechos y 100% de proyectos entregados en tiempo."
+        }
+      },
       "contact_us":{
         "getintouch":"Ponte en contacto",
         "post_sent":"Nos pondremos en contacto en un lapso menor a 24 horas.",
         "instructions":"Ingresa tu información, y nos pondremos en contacto contigo en menos de 24 horas.",
-        "our_office":"Nuestras Oficinas"
+        "our_office":"Nuestras Oficinas",
+        "visit_us":"visitanos en:"
       },
       "about_us": {
         "summary": "Somos una empresa de desarrollo y consultoría respaldada por profesionales con más de 10 años de " +
