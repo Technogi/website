bf964fa37b1f479a82660b6338ea2382e14d5ca0
ricardoMogg <ricardo@technogi.com.mx>
2015-01-28 12:49:12 -0600

    nuevos templates

---
+++ src/scripts/templates/methodology.js
@@ -0,0 +1,22 @@
+module.exports = [{
+  "name":"methodology",
+  "title":"IT Consulting",
+  "subtitle":"Hacemos consultoría IT",
+  "description":"<p>Utilizamos las mejores prácticas del PMI y metodologías de desarrollo "+
+                "ágil. Con esto logramos tener células de desarrollo autosuficientes que logran:"+
+                "<ul class='list'><li>Conocer al cliente de manera muy cercana.</li>
+                <li>Mejora la comunicación.</li>
+                <li>Aumenta la productividad.</li></ul>
+                <p>Minimizamos el riesgo, haciendo un análisis preliminar de la "+
+                "solución requerida, tratando de anticipar cualquier complicación. "+
+                "Este riesgo se mitiga:</p>"+
+                "<ul class='list'><li>Realizando validaciones de viabilidad técnica.</li>"+
+                  "<li>Levantamiento de requerimientos.</li>"+
+                  "<li>Revisión de Procesos de negocio.</li>"+
+                  "<li>Entrevistas con sponsors.</li></li>"+
+                  "<p>Aun así sabemos que es difícil prever todas las complicaciones, "+
+                  "por lo cual nos mantenemos flexibles al cambio. Aquí es donde entra "+
+                  "la parte ágil, donde respondemos a las nuevas necesidades del proyecto, "+
+                  "en tiempo real.</p>",
+  "layout":"text"
+}];
---
+++ src/scripts/templates/software_development.js
@@ -0,0 +1,63 @@
+module.exports = [{
+  "name":"software_development",
+  "title":"Software Development",
+  "subtitle":"We do Software Development",
+  "description":"<h3>¿Qué desarrollamos?</h3>"+
+                    "<ul class='list'><li>Aplicaciones Web y Empresariales.</li>"+
+                    "<li>Aplicaciones Móviles.</li>"+
+                    "<li>Integración de Sistemas.</li>"+
+                    "<li>Componentes de Negocio.</li>"+
+                    "<li>Prototipos para demostraciones y validación de ideas.</li></ul>"+
+                  "<h3>¿Cómo?</h3>"+
+                    "<p>El desarrollo de software presenta muchos desafíos en la "+
+                    "actualidad. Se requieren soluciones flexibles, que maximicen la "+
+                    "inversión en tecnología y que cumplan con la demanda constante de "+
+                    "calidad y rapidez en los tiempos de entrega.</p>"+
+                    "<p>En Technogi nos especializamos en cubrir todas estas necesidades.</p>"+
+                    "<ul class='list'><li><b>Amplio portafolio de opciones.</b> Nuestra filosofía nos "+
+                      "permite conocer las tecnologías más modernas e innovadoras y "+
+                      "hacernos especialistas en cada una de ellas.</li>"+
+                      "<li><b>Procesos de Calidad automatizados.</b> Con un desarrollo "+
+                      "orientado a pruebas, y la utilización de sistemas de integración "+
+                      "continua. Conocemos el estado de nuestro desarrollo en todo momento, "+
+                      "lo cual nos permite tomar decisiones al momento.</li>"+
+                      "<li><b>Visibilidad Total para el cliente. </b>En todo momento el cliente "+
+                      "conoce el estado actual de su desarrollo, utilizando reportes y "+
+                      "pizarrones de control.</li>"+
+                      "<li><b>Productos Palpables.</b> Con ciclos de desarrollo cortos y en "+
+                      "comunicación constante. Creamos software funcional que el cliente puede "+
+                      "ver y validar desde etapas muy temprana del proyecto, lo cual evita desvíos "+
+                      "con respecto a la idea original.</li>"+
+                      "<li><b>Supervisión de Expertos.</b> Nuestro equipo de arquitectos "+
+                      "establece los lineamientos para cada proyecto y sienta las bases para "+
+                      "que el equipo produzca de manera ágil y sin contratiempos. Al final de "+
+                      "cada entrega, se realizan validaciones nuevamente para garantizar que se "+
+                      "están siguiendo todos los lineamientos establecidos.</li></ul>"+
+                    "<p>Además cada uno de nuestros integrantes del equipo tiene un "+
+                    "perfil responsable y comprometido, lo cual nos ayuda a tener células "+
+                    "de desarrollo auto administradas, que impulsan la productividad y "+
+                    "permiten tener un uso eficiente de los recursos que manejan los proyectos.</p>"+
+                    "<p>Mantenemos informado al cliente en todo momento sobre el avance del "+
+                    "desarrollo y estimulamos la comunicación.</p>"+
+                  "<h3>Tecnologías</h3>"+
+                    "<ul class='table'><li>Angular JS</li><li>Java /Play Framework</li>"+
+                    "<li>Groovy / Grails</li><li>Ruby /Rails</li><li>Scala /Play Framework</li>"+
+                    "<li>Javascript</li><li>HTML5</li><li>CSS3</li><li>Foundation</li>"+
+                    "<li>Bootstrap</li></ul>"+
+                  "<h3>Bases de Datos</h3>"+
+                    "<ul class='table'><li>&nbsp;Redis</li><li>MongoDB</li>"+
+                    "<li>PostgreSQL</li><li>MySQL</li><li>Oracle</li>"+
+                    "<li>MSSQLServer</li></ul>"+
+                  "<h3>Herramientas</h3>"+
+                    "<ul class='table'><li>Jenkins</li><li>Sonar</li>"+
+                    "<li>Github</li><li>Bitbucket</li><li>Greenhopper</li>"+
+                    "<li>Confluence</li><li>JIRA</li><li>Jetbrains</li><li>Eclipse</li>"+
+                    "<li>Netbeans</li><li>Cordova / Phonegap</li></ul>"+
+                  "<h3>Algunos de nuestros proyectos</h3>"+
+                    "<ul class='table'><li>Angular JS</li><li>Java /Play Framework</li>"+
+                    "<li>Groovy / Grails</li><li>Ruby /Rails</li><li>Scala /Play Framework</li>"+
+                    "<li>Javascript</li><li>HTML5</li><li>CSS3</li><li>Foundation</li>"+
+                    "<li>Bootstrap</li></ul>",
+  "image":{"src":"http://placehold.it/350x350"},
+  "layout":"text"
+}];
