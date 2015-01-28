47172c47b47b67c50caf5156eefbecf101007a4d
Carlos Hernandez <carlos@technogi.com.mx>
2015-01-27 18:05:40 -0600

    messages

--- dist/msg/messages.js
+++ dist/msg/messages.js
@@ -1,65 +1,115 @@
 var messages = {
   "en": {
-    "commons":{
-      "learn_more":"learn more"
+    "commons": {
+      "learn_more": "learn more",
+      "sending":"sending ...",
+      "thankyou":"Thank you"
     },
     "views": {
-      "header":{
-        "services":"WHAT WE DO",
-        "methodology":"HOW WE DO IT",
-        "contact":"CONTACT  US"
+      "header": {
+        "services": "WHAT WE DO",
+        "methodology": "HOW WE DO IT",
+        "contact": "CONTACT  US"
       },
       "main": {
         "h1": "Modern IT Consulting Services",
-        "h2": "keep IT simple"
-      },"services":{
-        "h1":"Our Services",
-        "consulting":{
-          "title":"IT Consulting",
-          "description":
-            "Lorem ipsum dolor sit amet, id regione impedit " +
+        "h2": "keep IT simple",
+        "msg": "Focus on your business while we keep I.T. simple."
+      },
+      "services": {
+        "h1": "Our Services",
+        "consulting": {
+          "title": "IT Consulting",
+          "description": "Lorem ipsum dolor sit amet, id regione impedit " +
             "definitiones usu, velit persius percipit no duo."
         },
-        "project_landing":{
-          "title":"Project Landing",
-          "description":
-            "Servicio especializado en convertir ideas innovadoras "+
-            "y proyectos de alta complejidad en realidad. Utilizamos "+
-            "un proceso guiado que se adapta al estado actual de sus "+
+        "project_landing": {
+          "title": "Project Landing",
+          "description": "Servicio especializado en convertir ideas innovadoras " +
+            "y proyectos de alta complejidad en realidad. Utilizamos " +
+            "un proceso guiado que se adapta al estado actual de sus " +
             "necesidades y garantiza el éxito de la implementación."
         }
+
+      },
+      "about_us": {
+        "summary": "We are a consulting and development company built by professionals with over 10 years experience " +
+          "in implementing information technology solutions. Our commitment to research and innovate, allows us to " +
+          "provide our customers with simple solutions with the right technology, allowing them to focus on the " +
+          "productivity of their business."
+      },
+      "contact_us":{
+        "getintouch":"Get in touch",
+        "post_sent":"We'll get back to you within 24 hours. Hold on tight!",
+        "instructions":"Fill in the form below, and we'll get back to you within 24 hours.",
+        "our_office":"Our office"
+      },
+      "footer":{
+        "privacy_policy":"PRIVACY POLICY"
       }
     }
   },
   "es": {
-    "commons":{
-      "learn_more":"ver más"
+    "commons": {
+      "learn_more": "ver más",
+      "sending":"enviando ...",
+      "thankyou":"Gracias"
     },
     "views": {
-      "header":{
-        "services":"¿QUÉ HACEMOS?",
-        "methodology":"¿CÓMO LO HACEMOS?",
-        "contact":"CONTACTANOS"
+      "header": {
+        "services": "¿QUÉ HACEMOS?",
+        "methodology": "¿CÓMO LO HACEMOS?",
+        "contact": "CONTACTANOS"
       },
       "main": {
         "h1": "Consultoría de TI novedosa",
-        "h2": "keep IT simple"
-      },"services":{
-        "h1":"Nuestros servicios",
-        "consulting":{
-          "title":"Consultoría IT",
-          "description":
-            "Lorem ipsum dolor sit amet, id regione impedit " +
+        "h2": "keep IT simple",
+        "msg": "Concéntrate en tu negocio, nosotros nos preocupamos por T.I."
+      },
+      "services": {
+        "h1": "Nuestros servicios",
+        "consulting": {
+          "title": "Consultoría IT",
+          "description": "Lorem ipsum dolor sit amet, id regione impedit " +
             "definitiones usu, velit persius percipit no duo."
         },
-        "project_landing":{
-          "title":"Project Landing",
-          "description":
-            "Servicio especializado en convertir ideas innovadoras "+
-            "y proyectos de alta complejidad en realidad. Utilizamos "+
-            "un proceso guiado que se adapta al estado actual de sus "+
+        "software_development": {
+          "title": "Software Development",
+          "description": "Servicio especializado en convertir ideas innovadoras " +
+            "y proyectos de alta complejidad en realidad. Utilizamos " +
+            "un proceso guiado que se adapta al estado actual de sus " +
+            "necesidades y garantiza el éxito de la implementación."
+        },
+        "cloud": {
+          "title": "Cloud",
+          "description": "Servicio especializado en convertir ideas innovadoras " +
+            "y proyectos de alta complejidad en realidad. Utilizamos " +
+            "un proceso guiado que se adapta al estado actual de sus " +
+            "necesidades y garantiza el éxito de la implementación."
+        },
+        "project_landing": {
+          "title": "Project Landing",
+          "description": "Servicio especializado en convertir ideas innovadoras " +
+            "y proyectos de alta complejidad en realidad. Utilizamos " +
+            "un proceso guiado que se adapta al estado actual de sus " +
             "necesidades y garantiza el éxito de la implementación."
         }
+
+      },
+      "contact_us":{
+        "getintouch":"Ponte en contacto",
+        "post_sent":"Nos pondremos en contacto en un lapso menor a 24 horas.",
+        "instructions":"Ingresa tu información, y nos pondremos en contacto contigo en menos de 24 horas.",
+        "our_office":"Nuestras Oficinas"
+      },
+      "about_us": {
+        "summary": "Somos una empresa de desarrollo y consultoría respaldada por profesionales con más de 10 años de " +
+          "experiencia en la implementación de tecnologías de la información. Nuestra dedicación a la investigación e " +
+          "innovación nos permite entregar a nuestros clientes soluciones simples con la tecnología adecuada, " +
+          "permitiéndoles enfocarse en la productividad de su negocio."
+      },
+      "footer":{
+        "privacy_policy":"AVISO DE PRIVACIDAD"
       }
     }
   }
