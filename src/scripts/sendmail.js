5c3e0ce376e0c540c4c8de0a8a7080e1c0248b3d
Carlos Hernandez <carlos@technogi.com.mx>
2015-02-09 17:15:52 -0600

    lklk

--- src/scripts/sendmail.js
+++ src/scripts/sendmail.js
@@ -5,19 +5,42 @@
  * on 1/27/15
  */
 
-function sendmail(cb){
-  setTimeout(function(){
+function sendmail(cb) {
+  setTimeout(function () {
     cb();
-  },2000);
+  }, 2000);
 }
 
-exports.bind=function(){
-  $("#send_btn").on('click',function(e){
+exports.bind = function () {
+  $("#send_btn").on('click', function (e) {
     $("#contact_form").fadeOut(300);
     $(".sending_email").fadeIn(500);
-    sendmail(function(){
-      $(".sending_email").fadeOut(300);
-      $(".email_sent").fadeIn(500);
+    sendmail(function () {
+
+      var data = {
+        email: $("#contact_form_email").val(),
+        name: $("#contact_form_name").val(),
+        message: $("#contact_form_msg").val()
+      };
+
+
+      $.ajax({
+        url: "http://postmail.io:3080/contact/technogi",
+        type: "POST",
+        crossDomain: true,
+        data: JSON.stringify(data),
+        dataType: "json",
+        success: function (response) {
+          var resp = (response)
+          $(".sending_email").fadeOut(300);
+          $(".email_sent").fadeIn(500);
+        },
+        error: function (xhr, status) {
+          $(".sending_email").fadeOut(300);
+          $(".email_sent").fadeIn(500);
+        }
+      });
+
     });
     e.preventDefault();
   });
